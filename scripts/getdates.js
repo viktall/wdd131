9b8be9a8b3af679467b49b8ec4e860e8dcc0b959
unknown <viktall007@gmail.com>
2024-09-20 18:17:06 +0100

    first commit wdd131

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+const today = new Date();
+const year = document.querySelector("#year");
+year.innerHTML = `&copy; <span class="currentyear">${today.getFullYear()}</span> 🎈Victor Ugwu🎈 Lagos, Nigeria`;
+
+
